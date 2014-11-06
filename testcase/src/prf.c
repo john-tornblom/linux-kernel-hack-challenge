a14bb250d80cfdde11f9ebab858350d7e5adc888
John Törnblom <john.tornblom@gmail.com>
2014-11-06 20:40:17 +0100

    pfr: added ctor and dtor placeholders for setting up kernel <> userspace connection

--- testcase/src/prf.c
+++ testcase/src/prf.c
@@ -6,7 +6,7 @@
 #define US_DIFF(start , stop) (SEC2NANO *(stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec))
 
 static struct timespec t_start;
-
+static int fd;
 
 void START_PROFILING(uint64_t Interrupt_At_US, uint64_t General_Timeout_US) {
   clock_gettime(CLOCK_REALTIME, &t_start);
@@ -19,4 +19,11 @@ int  STOP_PROFILING(uint64_t* Measured_Time_US) {
   *Measured_Time_US = US_DIFF(t_start, t_stop);
 
   return 0;
+}
+
+__attribute__((constructor)) static void prf_constructor (void) {
+  fd = 1;
+}
+__attribute__((destructor)) static void prf_destructor (void) {
+  fd = -1;
 }
