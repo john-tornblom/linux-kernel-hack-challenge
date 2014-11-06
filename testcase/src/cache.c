b30b9b31de48c6e3d23e54e673b431f1ffb7b965
John Törnblom <john.tornblom@gmail.com>
2014-11-06 20:39:44 +0100

    cache: added ctor and dtor placeholders for setting up kernel <> userspace connection

--- testcase/src/cache.c
+++ testcase/src/cache.c
@@ -1,6 +1,27 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include "cache.h"
+
+#define PROC_PATH "/proc/Invalidate_L1"
+static int fd;
+
 
 void INVALIDATE_L1(void) {
 
 }
 
+
+__attribute__((constructor)) static void cache_constructor (void) {
+  fd = open(PROC_PATH, O_WRONLY);
+  if(fd < 0) {
+    perror("testcase: Unable to open "PROC_PATH);
+  }
+}
+
+__attribute__((destructor)) static void cache_destructor (void) {
+  if(fd >= 0) {
+    close(fd);
+  }
+}
