ae58d3a7c20e799da75ae926898dcc3a01c2f641
John Törnblom <john.tornblom@gmail.com>
2014-11-06 23:04:25 +0100

    cache: use open/close rather than fopen

--- testcase/src/cache.c
+++ testcase/src/cache.c
@@ -5,24 +5,24 @@
 #include "cache.h"
 
 #define PROC_PATH "/proc/Invalidate_L1"
-static FILE *pFile;
+static int fd;
 
 
 void INVALIDATE_L1(void) {
-  fprintf(pFile, "%d", 1);
+  write(fd, "\x00", 1);
 }
 
 
 __attribute__((constructor(102))) static void cache_constructor (void) {
-  pFile = fopen("/proc/Invalidate_L1", "w");
-  if(!pFile) {
+  if((fd = open(PROC_PATH, O_WRONLY | O_DSYNC | O_RSYNC | O_SYNC)) < 0) {
     perror("testcase: Unable to open "PROC_PATH);
   }
 }
 
+
 __attribute__((destructor)) static void cache_destructor (void) {
-  if(pFile) {
-    fclose(pFile);
+  if(fd != -1) {
+    close(fd);
   }
 }
 
